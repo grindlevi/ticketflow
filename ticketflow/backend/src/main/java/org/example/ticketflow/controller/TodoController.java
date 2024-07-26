979a5c385fdf1e9acdc5084ed7d749d636e0124d
grindlevi <grindlevi@gmail.com>
2024-07-26 15:19:08 +0200

    added basic crud operations for Todos

--- ticketflow/backend/src/main/java/org/example/ticketflow/controller/TodoController.java
+++ ticketflow/backend/src/main/java/org/example/ticketflow/controller/TodoController.java
@@ -2,9 +2,10 @@ package org.example.ticketflow.controller;
 
 import org.example.ticketflow.model.Todo;
 import org.example.ticketflow.service.TodoService;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class TodoController {
@@ -18,4 +19,25 @@ public class TodoController {
     public Todo createTodo(@RequestBody Todo todo) {
         return todoService.createTodo(todo);
     }
+
+    @GetMapping("/todos")
+    public List<Todo> getAllTodos() {
+        return todoService.getAllTodos();
+    }
+
+    @GetMapping("/todos/{id}")
+    public Optional<Todo> getTodoById(@PathVariable("id") Long id) {
+        return todoService.getTodo(id);
+    }
+
+    @DeleteMapping("/todos/{id}")
+    public void deleteTodo(@PathVariable("id") Long id) {
+        todoService.deleteTodo(id);
+    }
+
+    @PatchMapping("/todos/{id}")
+    public Todo updateTodo(@PathVariable("id") Long id, @RequestBody Todo todo) {
+        return todoService.updateTodo(id, todo);
+    }
 }
+
