2878a4a62e061cbe327ebf72c96ab9d6ab5818b6
grindlevi <grindlevi@gmail.com>
2024-08-08 13:31:47 +0200

    removed test comment to start development branch

--- ticketflow/backend/src/main/java/org/example/ticketflow/TicketflowApplication.java
+++ ticketflow/backend/src/main/java/org/example/ticketflow/TicketflowApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class TicketflowApplication {
-    // test comment to check CI workflow
     public static void main(String[] args) {
         SpringApplication.run(TicketflowApplication.class, args);
     }
