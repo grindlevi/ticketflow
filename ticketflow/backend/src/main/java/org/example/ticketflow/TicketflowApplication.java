d27b2cd8aebdb6fbd3ba004ab174c916d066ed36
grindlevi <grindlevi@gmail.com>
2024-08-08 15:13:05 +0200

    testing new automated merge#2

--- ticketflow/backend/src/main/java/org/example/ticketflow/TicketflowApplication.java
+++ ticketflow/backend/src/main/java/org/example/ticketflow/TicketflowApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class TicketflowApplication {
-    // comment to trigger ci
     public static void main(String[] args) {
         SpringApplication.run(TicketflowApplication.class, args);
     }
