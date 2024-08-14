635f4ac177c93780ab73196f5a3f88d134189b6b
grindlevi <134206215+grindlevi@users.noreply.github.com>
2024-08-14 17:56:47 +0200

    updated ticket creation to a popup modal

--- ticketflow/frontend/ticketflow-frontend/src/pages/Dashboard.tsx
+++ ticketflow/frontend/ticketflow-frontend/src/pages/Dashboard.tsx
@@ -1,12 +1,25 @@
+import { useState } from "react";
 import AddTicketForm from "../components/AddTicketForm";
 import MainBoard from "../components/MainBoard";
+import Modal from "../components/Modal";
 const Dashboard = () => {
+  const [showModal, setShowModal] = useState<boolean>(false);
+
+  const toggleModal = (): void => {
+    setShowModal(!showModal);
+  };
+
   return (
     <>
       <div className="dashboard-main">
         <h1>Dashboard</h1>
-      <AddTicketForm />
-      <MainBoard />
+        <div className="addTicket">
+          <button type="button" onClick={toggleModal}>+</button>
+        </div>
+        <MainBoard />
+        <Modal show={showModal} onClose={toggleModal}>
+          <AddTicketForm />
+        </Modal>
       </div>
     </>
   );
