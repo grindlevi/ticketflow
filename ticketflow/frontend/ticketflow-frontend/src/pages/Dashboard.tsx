455b33323489ad153ec9679aeb101ccf797254a0
grindlevi <134206215+grindlevi@users.noreply.github.com>
2024-08-28 14:58:25 +0200

    Refactored dashboard into smaller components

--- ticketflow/frontend/ticketflow-frontend/src/pages/Dashboard.tsx
+++ ticketflow/frontend/ticketflow-frontend/src/pages/Dashboard.tsx
@@ -3,56 +3,56 @@ import AddTicketForm from "../components/AddTicketForm";
 import MainBoard from "../components/MainBoard";
 import Modal from "../components/Modal";
 import { Ticket } from "../utils/types";
+
 const Dashboard = () => {
   const [showModal, setShowModal] = useState<boolean>(false);
-  const [tickets, setTickets] = useState<Ticket[] | []>([])
+  const [tickets, setTickets] = useState<Ticket[] | []>([]);
 
   const toggleModal = (): void => {
     setShowModal(!showModal);
   };
 
   const handleAddTicket = (newTicket: Ticket) => {
-    setTickets([...tickets, newTicket])
-  }
+    setTickets([...tickets, newTicket]);
+  };
 
   useEffect(() => {
-    const getTickets = async() => {
-      const token = localStorage.getItem('jwt')
-      const username = localStorage.getItem('username')
+    const getTickets = async () => {
+      const token = localStorage.getItem("jwt");
+      const username = localStorage.getItem("username");
       try {
         const response: Response = await fetch(`/api/todos/${username}`, {
           headers: {
-            'Authorization': `Bearer ${token}`
-          }
-        })
-  
-        if(!response.ok){
-          throw new Error("Fetching tickets failed.")
+            Authorization: `Bearer ${token}`,
+          },
+        });
+
+        if (!response.ok) {
+          throw new Error("Fetching tickets failed.");
         }
-  
-        const tickets: Ticket[] | [] = await response.json()
-        setTickets(tickets)
+
+        const tickets: Ticket[] | [] = await response.json();
+        setTickets(tickets);
         console.log(tickets);
-        
-  
       } catch (error) {
-        console.error('Error fetching tickets: ', error);
-        
+        console.error("Error fetching tickets: ", error);
       }
-    }
-    getTickets()
-  }, [])
+    };
+    getTickets();
+  }, []);
 
   return (
     <>
       <div className="dashboard-main">
         <h1>Dashboard</h1>
         <div className="addTicket">
-          <button type="button" onClick={toggleModal}>+</button>
+          <button type="button" onClick={toggleModal}>
+            +
+          </button>
         </div>
-        <MainBoard tickets={tickets} setTickets={setTickets}/>
+        <MainBoard tickets={tickets} setTickets={setTickets} />
         <Modal show={showModal} onClose={toggleModal}>
-          <AddTicketForm onFormSubmit={handleAddTicket}/>
+          <AddTicketForm onFormSubmit={handleAddTicket} />
         </Modal>
       </div>
     </>
